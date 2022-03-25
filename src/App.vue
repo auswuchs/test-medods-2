944ac2adaed3aadc30fd8cbb71e2b01390e33133
auswuchs <oxotnik3330@gmail.com>
2022-03-25 11:26:38 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main class="main">
+    <TheWelcome />
+  </main>
+</template>
+
+<style lang="scss">
+#app {
+  max-width: 1280px;
+  min-height: 100vh;
+  max-height: 100vh;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+.main {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  height: 70vh;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+  <div class="wrapper">
+    <button class="panel t-l-panel"></button>
+    <button class="panel t-r--panel"></button>
+    <button class="panel b-l-panel"></button>
+    <button class="panel b-r-panel"></button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  display: grid;
+  max-width: 250px;
+  max-height: 250px;
+  grid-template-columns: repeat(2, 100px);
+  grid-template-rows: repeat(2, 100px);
+  background-color: aliceblue;
+  border-radius: 50%;
+}
+
+.panel {
+  justify-self: center;
+  width: 100px;
+  height: 100px;
+  border: none;
+  background-color: aqua;
+}
+
+.t-l-panel {
+  background: dodgerblue;
+  opacity: 0.6;
+}
+</style>
